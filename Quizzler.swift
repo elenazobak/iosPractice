e804c9f9490ac48a727af6ff27f6908082e7a296
Elena Zobak <elenazobak@gmail.com>
2022-12-27 16:25:33 -0500

    quizer2

--- Quizzler.swift
+++ Quizzler.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,15 +7,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
+        
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        questionLable.text = quiz[questionNumber]
     }
     
 }
-
