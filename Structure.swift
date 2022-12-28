015a7c110197a63171d25578fe229dda5570d13f
Elena Zobak <elenazobak@gmail.com>
2022-12-28 15:37:53 -0500

    structures2

--- Structure.swift
+++ Structure.swift
@@ -1,5 +1,22 @@
+//new Object /clas?
 struct Town {
-    let name = "Elena"
+    let name = "Angeland"
     var citizens = ["Angela","Jack Bauer"]
     var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+
+
+    //method - a function inside a class or a struct
+    func fortify() {
+        print("Defences increased!")
+    }
 }
+
+var myTown = Town()
+
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["Grain"]!) bags of grain. ") // Angeland has 100 bags of grain
+
+myTown.citizens.append("Keanu Reeves")// add to an array
+print(myTown.citizens.count)
+
+myTown.fortify()
