8560367eba12e78f9e72db40908f935bc9407649
Elena Zobak <elenazobak@gmail.com>
2023-01-05 06:05:54 -0500

    quiz question

---
+++ Quizzler/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Elena Zobak on 12/29/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+    text = q
+        answer = a
+    }
+    
+}
