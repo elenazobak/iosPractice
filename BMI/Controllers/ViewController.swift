5a988c0ae9756cd37a39960ac124a6bcc1ea4291
Elena Zobak <elenazobak@gmail.com>
2023-01-15 15:53:40 -0500

    secondViewController

--- BMI/Controllers/ViewController.swift
+++ BMI/Controllers/ViewController.swift
@@ -43,6 +43,10 @@ class ViewController: UIViewController {
         //let bmi = weight / (height * height)
         let bmi = weight/pow(height,2)
         print(bmi)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
