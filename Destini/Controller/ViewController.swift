04a6dd80ccde86a1272817c0035dfc928950fa18
Elena Zobak <elenazobak@gmail.com>
2023-01-23 11:31:15 -0500

    desti

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,24 +6,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [
-        Story(s: "You see a fork in the road", c1: "Take a left", c2: "Take a right"),
-        Story(s: "You see a tiger.", c1: "Shout for help", c2: "Play dead"),
-        Story(s: "You find a treasure chest", c1: "Open it", c2: "Check for traps")
-    ]
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        storyLabel.text = stories[0].story
+        
+        updateUI()
 
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-    
-    
-    
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+     
     }
     
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
     
 }
-
