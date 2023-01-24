e711a5d28adf6fa1709b53c129f86b9ed6939513
Elena Zobak <elenazobak@gmail.com>
2023-01-24 10:44:50 -0500

    Tipsy pr

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
 import UIKit
 
 class CalculatorViewController: UIViewController {
@@ -6,23 +14,46 @@ class CalculatorViewController: UIViewController {
    
     @IBOutlet weak var zeroPctButton: UIButton!
     
-    
     @IBOutlet weak var tenPctButton: UIButton!
     
     @IBOutlet weak var twentyPctButton: UIButton!
     
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    
-
-
+    var tip = 0.10
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        //Make the button that triggered the IBAction selected.
+       sender.isSelected = true
+                
+        //Get the current title of the button that was pressed.
+        let buttonTitle = sender.currentTitle!
         
+        //Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        
+        //Turn the String into a Double.
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
+        tip = buttonTitleAsANumber / 100
+        
+//        switch buttonTitle {
+//        case "0%": prctSelected = 0
+//        case "20%": prctSelected = 20
+//        case "10%": prctSelected = 10
+//        default:
+//            print("Error")
+//
+//        }
         
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tip)
     }
     
 }
---
+++ stringFunc.swift
@@ -0,0 +1,11 @@
+ //Get the current title of the button that was pressed.
+        let buttonTitle = sender.currentTitle!
+        
+        //Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        
+        //Turn the String into a Double.
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        
+        //Divide the percent expressed out of 100 into a decimal e.g. 10 becomes 0.1
+        tip = buttonTitleAsANumber / 100
