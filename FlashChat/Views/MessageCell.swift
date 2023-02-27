9de43952cb9381b09766aec715c5728bed70f793
Elena Zobak <elenazobak@gmail.com>
2023-02-27 10:26:07 -0500

    chat final

--- FlashChat/Views/MessageCell.swift
+++ FlashChat/Views/MessageCell.swift
@@ -1,27 +1,17 @@
-//
-//  MessageCell.swift
-//  Flash Chat iOS13
-//
-//  Created by Elena Zobak on 2/17/23.
-//  Copyright © 2023 Angela Yu. All rights reserved.
-//
-
 import UIKit
 
 class MessageCell: UITableViewCell {
-    
-    
-    
+
     @IBOutlet weak var messageBubble: UIView!
-    @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
     }
-  
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
